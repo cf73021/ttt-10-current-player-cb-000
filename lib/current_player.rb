5d3b2053d5b3cd34eee94fdbfb0cca3438383caa
Chris Franklin <cf73021@gmail.com>
2018-07-02 21:15:30 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,9 +9,9 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board)  % 2
+  if turn_count(board) % 2
     return "X"
-  else
+  elsif turn_count(board) !% 2
      return "O"
   end
 end
