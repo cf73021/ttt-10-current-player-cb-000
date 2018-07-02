7a9fa7ee776631c3508207da3afb5dc773962886
Chris Franklin <cf73021@gmail.com>
2018-07-02 20:57:21 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,9 @@
 def turn_count(board)
   counter = 0
   board.each do |turn|
-    puts "This is turn number #{turn}"
-    counter += 1
+    if turn == "X" || "O"
+      counter += 1
+    end
+  puts "This is turn number #{turn}"
   end
-  end
+end
